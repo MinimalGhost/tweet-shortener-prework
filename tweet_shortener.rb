788f88ce51f42b939f735fcd30710a95bf6b78e9
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-12 00:55:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,7 @@ def shortened_tweet_truncator(tweet)
   if tweet.length >= 140
     word_substituter(tweet)
   elsif word_substituter(tweet).length > 140
-    "#{tweet[0..140]}..."
+    "#{tweet[0..140]}."
   else
     tweet
   end
